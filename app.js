fefa042a70c58632e41be2687480f5520242d5bb
Phillip Mispagel <pmispagel@turnitin.com>
2017-11-14 16:03:28 -0800

    adding and fetching users now using rethinkdb and koa async / await

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const Koa = new require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 
+const r = require('rethinkdbdash')();
+
+r.db('test').tableList().run().then((tables) => {
+  console.log(tables);
+  if (!tables.includes('users')) {
+    r.tableCreate('users').run();
+  }
+});
+
 const koa = new Koa();
 const router  = new Router();
 
@@ -15,19 +24,37 @@ function getMessage(ctx, next) {
   ctx.body = 'Hello World Message!';
 }
 
-function addUser(ctx, next) {
+async function addUser(ctx, next) {
   console.log(ctx.request.body);
+
   let {
     email,
     password
   } = ctx.request.body;
 
-  ctx.body = `recieved ${email} and ${password}!`;
+  try {
+    let user = await r.table('users').insert({
+      email,
+      password
+    });
+
+    ctx.body = user;
+  }
+  catch(e) {
+    ctx.status = 500;
+    ctx.body = e.message || 'whoops!';
+  }
 }
 
-function listUsers(ctx, next) {
-  console.log(ctx.request);
-  ctx.body = 'all users';
+async function listUsers(ctx, next) {
+  try {
+    let users = await r.table('users');
+    ctx.body = users;
+  }
+  catch(e) {
+    ctx.status = 500;
+    ctx.body = e.message || 'whoops!';
+  }
 }
 
 koa.use(router.routes());
